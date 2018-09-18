d7e8b3944adec233bf4dfaf458c146852a994f7e
Runone <ngo.danny11@gmail.com>
2018-09-17 20:30:23 -0700

    First commit

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -7,4 +7,31 @@
 var getElementsByClassName = function(className
 ) {
   // your code here
+//get the classes from document.body
+//get the classnames from the function
+//return he the classnames with recursive
+
+var newResult = [];
+var body = document.body;
+var findElement = function (element) {
+  
+  if (!element.classList) {
+    return 
+  } 
+  else {
+  element.classList.forEach(function(name){
+    if(name === className) {
+      newResult.push(element);
+    }
+    
+  })
+  element.childNodes.forEach(function(child) {
+    findElement(child);
+  });
+  }
+}
+
+findElement(body);
+
+return newResult;
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,57 @@
 
 // but you don't so you're going to write it from scratch:
 
+//need to satisfy these below conditions
+//case for null
+//case for number
+//case for boolean
+//case for string
+//case for array
+//case for embedded array
+//case for object
+//case for object embedded array and object
+//case for undefined
+//case for functions
+
 var stringifyJSON = function(obj) {
-  // your code goes here
+  let results = [];
+  console.log(obj);
+  //when obj is null
+  if (obj === null) {
+    return 'null';
+  }
+
+  //when obj is number or boolean
+  if (typeof obj === 'number' || typeof obj === 'boolean') {
+    return obj.toString();
+  }
+
+  //when obj is a string
+  if (typeof obj === 'string') {
+    return "\"" + obj + "\"" ; 
+  }
+
+  //when obj is an array
+  if (Array.isArray(obj) && obj.length === 0) {
+      results += '[]';
+      return results
+    }
+      if (Array.isArray(obj) && obj.length > 0) {
+      var newArray = [];
+      obj.forEach(element => {
+        newArray.push(stringifyJSON(element));
+      });
+      results += '[' + newArray.join(',') + ']'
+      return results
+    }  
+  //when obj is an object
+  for (let key in obj) {
+      if (typeof obj[key] !== undefined && typeof obj[key] !== 'function') {
+    results.push(stringifyJSON(key) + ':' + stringifyJSON(obj[key]));
+  }
+
+  }
+  var newResults = '{' + results.join() + '}';
+  return newResults;
+
 };
